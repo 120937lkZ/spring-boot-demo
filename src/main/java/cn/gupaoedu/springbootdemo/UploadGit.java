039518b5fc5a83607bdb08c190c2cfc666d0420e
120937lkZ <nyken_likezhou@163.com>
2020-08-11 22:58:19 +0800

    代码仓修改

---
+++ src/main/java/cn/gupaoedu/springbootdemo/UploadGit.java
@@ -0,0 +1,10 @@
+package cn.gupaoedu.springbootdemo;
+
+public class UploadGit {
+
+    private int i = 0 ;
+
+    public static void main(String[] args) {
+        System.out.println("上传到远程Git仓库");
+    }
+}
